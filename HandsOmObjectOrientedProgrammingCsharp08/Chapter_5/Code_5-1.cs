ab07af90f61b0d86f60baa20a0e674258a3111e4
Jorge Baltodano <george_itec02@hotmail.com>
2020-09-14 21:02:39 -0500

    Cambios Finales
    Todo el contenido sobre el libro

---
+++ HandsOmObjectOrientedProgrammingCsharp08/Chapter_5/Code_5-1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Chapter5
+{
+    class Code_5_1
+    {
+        public static void Main()
+        {
+            int[] a = { 1, 2, 3, 4 };
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
---
+++ HandsOmObjectOrientedProgrammingCsharp08/Chapter_9/MetodoGenerico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp08_Genericos
+{
+    class MetodoGenerico
+    {
+
+        public static T Maximo<T>(T valueA, T valueB) where T : IComparable<T>
+        {
+            return valueA.CompareTo(valueB) > 0 ? valueA:valueB;
+        }
+    }
+}
